de3e36f664b59e8e8bf7436a915c5c082c53d5e2
Ingot Farming <ingotfarming@gmail.com>
2021-06-13 21:24:48 +0200

    Added commands

--- scripts/mint_token_X.js
+++ scripts/mint_token_X.js
@@ -3,10 +3,12 @@
 const IngotToken = artifacts.require("IngotToken");
 
 module.exports = async function (callback) {
+  console.log("param: " + process.argv[4]);
   const ingotToken = await IngotToken.deployed();
   const address = await ingotToken.owner();
   console.log("owner: "+ address);
-  await ingotToken.mint(address, web3.utils.toWei("1050000"));
-  console.log("Minted tokens");
+  let weiAmount = web3.utils.toWei(process.argv[4]);
+  await ingotToken.mint(address, weiAmount);
+  console.log("Minted tokens: "+ weiAmount);
   callback();
 };
--- scripts/set_halves_to_X.js
+++ scripts/set_halves_to_X.js
@@ -3,8 +3,10 @@
 const IngotFarm = artifacts.require("IngotFarm");
 
 module.exports = async function (callback) {
+  console.log("param: " + process.argv[4]);
   const pool = await IngotFarm.deployed();
-  let weiAmount = web3.utils.toWei("20");
+  let weiAmount = web3.utils.toWei(process.argv[4]);
+  await pool.updatePool();
   await pool.setRewardPerBlock(weiAmount);
   console.log("set reward: ", weiAmount);
   callback();
