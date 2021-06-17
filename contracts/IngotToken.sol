c75269ebfef4e740a935d244bfd5a7356c038d1a
Ingot Farming <ingotfarming@gmail.com>
2021-06-17 13:39:14 +0200

    fix liq

--- contracts/IngotToken.sol
+++ contracts/IngotToken.sol
@@ -13,7 +13,7 @@ contract IngotToken is ERC20, IIngotToken, Ownable {
 
     // uint256 public UNISWAP_LIQUIDITY = 1050000 * 1e18;
     mapping (address => bool) public _minters;
-    constructor() public ERC20("Ingot Token", "Ingot"){
+    constructor() public ERC20("Ingot Token", "INGOT"){
         _minters[msg.sender] = true;
         // _mint(msg.sender, UNISWAP_LIQUIDITY);
     }
